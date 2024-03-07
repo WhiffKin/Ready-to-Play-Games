e9766575579dad2a60b4c49f07c1e189460b4e28
WhiffKin <garrettlackey2018@gmail.com>
2024-03-07 13:12:32 -0600

    Styled Nav Bar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,32 +4,38 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/characters">Characters</NavLink>
-      </li>
-      <li>
-        <NavLink to="/characters/new">Create Character</NavLink>
-      </li>
-      <li>
-        <NavLink to="/templates">Campaign Templates</NavLink>
-      </li>
-      <li>
-        <NavLink to="/templates/new">Create Campaign Template/Rooms</NavLink>
-      </li>
-      <li>
-        <NavLink to="/campaigns">Campaigns</NavLink>
-      </li>
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <div>
+          <li>
+            <NavLink to="/users">Users</NavLink>
+          </li>
+          <li>
+            <NavLink to="/characters">Characters</NavLink>
+          </li>
+          <li>
+            <NavLink to="/characters/new">Create Character</NavLink>
+          </li>
+          <li>
+            <NavLink to="/templates">Campaign Templates</NavLink>
+          </li>
+          <li>
+            <NavLink to="/templates/new">Create Campaign Template/Rooms</NavLink>
+          </li>
+          <li>
+            <NavLink to="/campaigns">Campaigns</NavLink>
+          </li>
+        </div>
+        <div>
+          <li>
+            <ProfileButton />
+          </li>
+        </div>
+      </ul>
+    </nav>
   );
 }
 
